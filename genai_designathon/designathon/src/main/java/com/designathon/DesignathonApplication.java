fd8ed9d330b7c55bc2e3285259945fa202cc5e90
itzjana <janakamalakannan27@gmail.com>
2026-08-01 20:37:54 +0530

    feat: initialize Spring Boot project structure with Maven wrapper configuration

---
+++ genai_designathon/designathon/src/main/java/com/designathon/DesignathonApplication.java
@@ -0,0 +1,13 @@
+package com.designathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignathonApplication.class, args);
+	}
+
+}
---
+++ genai_designathon/designathon/src/test/java/com/designathon/DesignathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
